1c0d41574f3550f1135cd20928afd56eea4228ef
Raphael Negrisoli Batista <raphaelnegrisoli@gmail.com>
2013-03-22 15:51:08 -0300

    Fix zsh and gradle instalation.

--- install_java.sh
+++ install_java.sh
@@ -1,3 +1,7 @@
 # java 7
-sudo apt-get install openjdk-7-jdk junit4 junit4-doc gradle gradle-doc
+sudo apt-get install openjdk-7-jdk junit4 junit4-doc
+wget --no-ckeck-certificate http://services.gradle.org/distributions/gradle-1.4-all.zip -O -
+unzip gradle-1.4-all.zip
+sudo mv gradle-1.4 /opt/gradle/
+echo 'alias gg="/opt/gradle/bin/gradle --daemon"' >> ~/.profile
 sudo update-java-alternatives -s java-1.7.0-openjdk-i386
