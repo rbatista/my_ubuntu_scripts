7c3f1ddef1ffa884382c352bb0142d9d954cdffe
Raphael Negrisoli Batista <raphaelnegrisoli@gmail.com>
2013-07-18 14:28:08 -0300

    Fix unzip. Put automatic version of java-7.

--- install_java.sh
+++ install_java.sh
@@ -1,7 +1,6 @@
 # java 7
 sudo apt-get install -y --quiet openjdk-7-jdk junit4 junit4-doc
-wget --no-check-certificate http://services.gradle.org/distributions/gradle-1.4-all.zip -O -
-unzip gradle-1.4-all.zip
+wget -qO- -O tmp.zip --no-check-certificate http://services.gradle.org/distributions/gradle-1.4-all.zip && unzip tmp.zip && rm tmp.zip
 sudo mv gradle-1.4 /opt/gradle/
 echo 'alias gg="/opt/gradle/bin/gradle --daemon"' >> ~/.profile
-sudo update-java-alternatives -s java-1.7.0-openjdk-i386
+sudo update-java-alternatives -s $(update-java-alternatives -l | grep "java-1.7" | cut -d ' ' -f1)
