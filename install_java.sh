ae0c69ffc5bdc22bf043d3db857c93090ace3d34
Raphael Negrisoli Batista <raphaelnegrisoli@gmail.com>
2013-07-18 12:21:21 -0300

    Add yes to java installation

--- install_java.sh
+++ install_java.sh
@@ -1,5 +1,5 @@
 # java 7
-sudo apt-get install openjdk-7-jdk junit4 junit4-doc
+sudo apt-get install -y -quiet openjdk-7-jdk junit4 junit4-doc
 wget --no-ckeck-certificate http://services.gradle.org/distributions/gradle-1.4-all.zip -O -
 unzip gradle-1.4-all.zip
 sudo mv gradle-1.4 /opt/gradle/
