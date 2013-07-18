1a44fcbd7621e66af856c0a6268e4e0e4f3feb46
Raphael Negrisoli Batista <raphaelnegrisoli@gmail.com>
2013-07-18 13:56:34 -0300

    Fix parameter --no-check-certificate of wget.

--- install_java.sh
+++ install_java.sh
@@ -1,6 +1,6 @@
 # java 7
 sudo apt-get install -y --quiet openjdk-7-jdk junit4 junit4-doc
-wget --no-ckeck-certificate http://services.gradle.org/distributions/gradle-1.4-all.zip -O -
+wget --no-check-certificate http://services.gradle.org/distributions/gradle-1.4-all.zip -O -
 unzip gradle-1.4-all.zip
 sudo mv gradle-1.4 /opt/gradle/
 echo 'alias gg="/opt/gradle/bin/gradle --daemon"' >> ~/.profile
