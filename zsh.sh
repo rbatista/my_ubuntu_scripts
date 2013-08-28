69e7e59fd351a856268b83266cf8b46fe135e562
Raphael Negrisoli Batista <raphaelnegrisoli@gmail.com>
2013-08-28 15:08:23 -0300

    Add zsh intalation.
    Include:
     - git
     - Oh-my-zsh
     - solarized theme

---
+++ zsh.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# pre-requisite
+sudo apt-get install git -y
+
+#zsh
+sudo apt-get install zsh -y
+
+# oh-my-zsh
+wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+
+# color solarized
+git clone git://github.com/sigurdga/gnome-terminal-colors-solarized.git ~/.terminal-solarized
+~/.terminal-solarized/solarize dark
