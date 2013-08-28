a9097a81a9ec040fb6764ab0d2b2961f1b19d14e
Raphael Negrisoli Batista <raphaelnegrisoli@gmail.com>
2013-08-28 15:37:44 -0300

    Update gradle version.
     1.4 => 1.7

--- java.sh
+++ java.sh
@@ -2,8 +2,8 @@
 
 # java 7
 sudo apt-get install -y --quiet openjdk-7-jdk junit4 junit4-doc
-wget -qO- -O tmp.zip --no-check-certificate http://services.gradle.org/distributions/gradle-1.4-all.zip && unzip tmp.zip && rm tmp.zip
-sudo mv gradle-1.4 /opt/gradle/
+wget -qO- -O tmp.zip --no-check-certificate http://services.gradle.org/distributions/gradle-1.7-all.zip && unzip tmp.zip && rm tmp.zip
+sudo mv gradle-1.7 /opt/gradle/
 echo 'alias gg="/opt/gradle/bin/gradle --daemon"' >> ~/.profile
 source ~/.profile
 sudo update-java-alternatives -s $(sudo update-java-alternatives -l | grep "java-1.7" | cut -d ' ' -f1)
