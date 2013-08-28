20cd05aebfb525e964272f4f1c313ff7c1f3f2dd
Raphael Negrisoli Batista <rbatista@desktop-01.(none)>
2013-08-28 15:59:47 -0300

    Add git parameters verification.

--- git.sh
+++ git.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
-sudo apt-get install git
-git config --global user.name "$1"
-git config --global user.email "$2"
+if [ -z "$1" -a -z "$2" ]; then
+    echo 'Usage: ./git.sh "user" "email@me.com"'
+    exit 0
+fi
+
+#sudo apt-get install git
+#git config --global user.name "$1"
+#git config --global user.email "$2"
 
 # Create a new ssh-key
-echo "Genereting a new ssh-key"
-ssh-keygen -t rsa -C "$2"
+#echo "Genereting a new ssh-key"
+#ssh-keygen -t rsa -C "$2"
 
 # copy the public key to clipboard
-sudo apt-get install xclip
+#sudo apt-get install xclip
 
-xclip -sel clip < ~/.ssh/id_rsa.pub
+#xclip -sel clip < ~/.ssh/id_rsa.pub
 
-echo "\nYour public key is in your clipboad."
+#echo "\nYour public key is in your clipboad."
