6e95af298ed7c55105f2b98f31267ce152652ada
Raphael Negrisoli Batista <raphaelnegrisoli@gmail.com>
2013-08-28 15:36:17 -0300

    Rename install to multimidia.
     Remove duplicate packages.

--- multimidia.sh
+++ multimidia.sh
@@ -1,14 +1,6 @@
+#!/usr/bin/env bash
+
 # multimidia packages
 sudo wget --output-document=/etc/apt/sources.list.d/medibuntu.list http://www.medibuntu.org/sources.list.d/$(lsb_release -cs).list && sudo apt-get --quiet update && sudo apt-get --yes --quiet --allow-unauthenticated install medibuntu-keyring && sudo apt-get --quiet update
 
 sudo apt-get install ubuntu-restricted-extras non-free-codecs libdvdcss2 faac faad ffmpeg ffmpeg2theora flac icedax id3v2 lame libflac++6 libjpeg-progs libmpeg3-1 mencoder mjpegtools mp3gain mpeg2dec mpeg3-utils mpegdemux mpg123 mpg321 regionset sox uudeview vorbis-tools x264 arj lha p7zip p7zip-full p7zip-rar rar unrar unace-nonfree vlc
-
-# essential compilation packages
-sudo apt-get install build-essential checkinstall cdbs devscripts dh-make fakeroot libxml-parser-perl check avahi-daemon -y
-
-# git
-sudo apt-get install git git-flow -y
-
-# teminal
-sudo apt-get install guake zsh -y
-wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
