e7587186b3b23257b5f16ac0c2f6558043d68c91
Raphael Negrisoli Batista <raphaelnegrisoli@gmail.com>
2013-08-28 15:42:52 -0300

    Generate ssh-key.
     The public key is copied to cliboard.

--- git.sh
+++ git.sh
@@ -3,3 +3,14 @@
 sudo apt-get install git
 git config --global user.name "$1"
 git config --global user.email "$2"
+
+# Create a new ssh-key
+echo "Genereting a new ssh-key"
+ssh-keygen -t rsa -C "$2"
+
+# copy the public key to clipboard
+sudo apt-get install xclip
+
+xclip -sel clip < ~/.ssh/id_rsa.pub
+
+echo "\nYour public key is in your clipboad."
