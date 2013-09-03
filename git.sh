588a6cb2c27743c960b6cd637b4610feb1a9af99
Raphael Negrisoli Batista <raphaelnegrisoli@gmail.com>
2013-09-03 14:25:21 -0300

    Add color to git script.

--- git.sh
+++ git.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+GREEN='\e[0;32m'
+RED='\e[0;31m'
+NO_COLOR='\e[0m'
+
 if [ -z "$1" -a -z "$2" ]; then
-    echo 'Usage: ./git.sh "user" "email@me.com"'
+    echo -e "${RED}Usage: ./git.sh \"user\" \"email@me.com\"${NO_COLOR}"
     exit 0
 fi
 
@@ -10,12 +14,11 @@ git config --global user.name "$1"
 git config --global user.email "$2"
 
 # Create a new ssh-key
-echo "\nGenereting a new ssh-key...\n"
+echo -e "\n${GREEN}Genereting a new ssh-key...${NO_COLOR}\n"
 ssh-keygen -t rsa -C "$2"
 
 # copy the public key to clipboard
 sudo apt-get install xclip
-
 xclip -sel clip < ~/.ssh/id_rsa.pub
 
-echo "\nYour public key is in your clipboad.\n"
+echo -e "\n${GREEN}Your public key is in your clipboad.${NO_COLOR}\n"
