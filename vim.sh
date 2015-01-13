17e82dfcc669bb7de75e010ed44869e463e7a45f
Raphael Negrisoli Batista <raphaelnegrisoli@gmail.com>
2015-01-13 16:54:04 -0200

    Create font directories.

--- vim.sh
+++ vim.sh
@@ -39,6 +39,8 @@ echo 'syntax enable' >> ~/.vimrc
 echo 'set background=dark' >> ~/.vimrc
 echo 'colorscheme solarized' >> ~/.vimrc
 echo 'set t_Co=256' >> ~/.vimrc
+echo 'let g:solarized_termcolors=256' >> ~/.vimrc
+
 echo -e '\n' >> ~/.vimrc
 
 # airline
@@ -47,8 +49,11 @@ echo -e '\n' >> ~/.vimrc
 # powerline fonts
 wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf
 wget https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
+mkdir -p ~/.fonts/
 mv PowerlineSymbols.otf ~/.fonts/
 fc-cache -vf ~/.fonts/
+
+mkdir -p ~/.config/fontconfig/conf.d/
 mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
 
 git clone https://github.com/bling/vim-airline ~/.vim/bundle/vim-airline
